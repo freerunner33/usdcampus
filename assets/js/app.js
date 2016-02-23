fb84ac1d310289f4799232d3526c92c808051113
Noah Thomas <noahthomas@Noahs-Turing-Machine.local>
2016-02-23 09:40:51 -0800

    USD Campus created

---
+++ assets/js/app.js
@@ -0,0 +1,32 @@
+
+$('img').on('click', function() {
+	alert('ready')
+})
+
+$(document).ready(function() {
+	// $.ajax({
+	// 	url: 'http://noahthomas.us/',
+	// 	method: 'POST'
+	// 	success: function(data, status) {
+
+	// 	},
+	// 	error: function() {
+			
+	// 	},
+	// 	complete: function() {
+			
+	// 	}
+	// })
+
+	$('button').on('click', function() {
+		if ('geolocation' in navigator) {
+	
+			navigator.geolocation.getCurrentPosition(function(position) {
+				alert('hi')
+				// do_something(position.coords.latitude, position.coords.longitude);
+			})
+		} else {
+			document.getElementById('location').innerHTML = 'Navigator is included'
+		}
+	})
+})
